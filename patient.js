3b7e1632bc8891fe9cfe908ab5d8122148dcd4f4
Jessica Orrick <jessica.orrick@outlook.com>
2024-07-18 09:55:27 -0500

    added while loop to create new classes

--- patient.js
+++ patient.js
@@ -1,9 +1,10 @@
 //what is included in a patient?
 const fs = require('fs')
+const input = require("readline-sync");
 
 class Patient {
     constructor(firstName, lastName, dateOfBirth, email, address) {
-        this.firstName = firstName;
+        this.firstName = firstName
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
         this.email = email;
@@ -11,10 +12,28 @@ class Patient {
     }
 }
 
-const patient1 = new Patient("jessica", "orrick", "6/2/85", "jessica@jessica.com", "123 Main Street")
-console.log(patient1)
+// For every PT
+let counter = 0
+let patients = {}
+let continueLoop = true
+while (continueLoop) {
+    let firstName = input.question("What is the client's first name? ")
+    let lastName = input.question("What is the client's last name? ")
+    let dateOfBirth = input.question("What is the client's date of birth (yyyy/mm/dd)? ")
+    let email = input.question("What is the client's email address? ")
+    let address = input.question("What is the client's street address? ")
 
-let data = JSON.stringify(patient1)
+    let patientName = `patient_${counter}`
+    patients[patientName] = new Patient(firstName, lastName, dateOfBirth, email, address)
+    continueInput = input.question("Would you like to add another patient? Y/N ")
+    counter += 1
+    if (continueInput.toLowerCase() === "n")
+        continueLoop = false
+}
+console.log(patients)
+
+
+let data = JSON.stringify(patients)
 
 
 fs.writeFile('test.txt', data, (err) => {
